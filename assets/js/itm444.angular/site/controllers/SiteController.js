e515fab93b1a7a7931625ec7c22302e7a266908b
Brian Jurgess <jurgessb@msu.edu>
2015-04-27 20:21:47 -0400

    Pushing latest fixes for initial data

--- assets/js/itm444.angular/site/controllers/SiteController.js
+++ assets/js/itm444.angular/site/controllers/SiteController.js
@@ -13,6 +13,7 @@
     function SiteController($scope, $rootScope, $routeParams, SiteDetailService) {
         $scope.siteId = $routeParams.siteId;
         $scope.site = {};
+        $scope.alerts = [];
 
         getSite();
 
@@ -21,10 +22,33 @@
 
             function getSiteSuccessFn(data, status, headers, config) {
                 $scope.site = data.data;
+
+                angular.forEach($scope.site.alerts, function(alert, index) {
+                   var temp = {
+                       title: alert.title
+                   };
+
+                    switch (alert.level) {
+                        case 0:
+                            temp.type = 'success';
+                            break;
+                        case 1:
+                            temp.type = 'warning';
+                            break;
+                        case 2:
+                            temp.type = 'danger';
+                            break;
+                    };
+                    $scope.alerts.push(temp);
+                });
             }
 
             function getSiteFailFn(data, status, headers, config) {
                 console.log("failed");
+            }
+
+            $scope.closeAlert = function(index) {
+                $scope.alerts.splice(index,1);
             }
         }
     }
